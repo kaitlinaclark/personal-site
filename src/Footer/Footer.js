3fbb7563275d1ccc125f85b923860e9adb247d8c
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-10-15 13:58:22 -0500

    everything works

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
                     <li className="nav-link">Connect with me!</li>
                 </ul>
                 <ul className="navbar-nav">
-                    <li><a className="nav-link fa fa-lg fa-instagram" href="instagram.com">  Instagram</a></li>
-                    <li><a className="nav-link fa fa-lg fa-github" href="github.com"> Github Repo</a></li>
-                    <li><a className="nav-link fa fa-lg fa-twitter" href="twitter.com">  Twitter</a></li>
+                    <li><a className="nav-link fa fa-lg fa-linkedin" href="https://www.linkedin.com/in/kaitlin-a-clark-187885a5/">  LinkedIn</a></li>
+                    <li><a className="nav-link fa fa-lg fa-github" href="https://github.com/kaitlinaclark"> Github Repo</a></li>
+                    <li><a className="nav-link fa fa-lg fa-twitter" href="https://twitter.com/kaitlinaclark">  Twitter</a></li>
                 </ul>
             </div>
         </nav>
--- src/files/list-of-projects.js
+++ src/files/list-of-projects.js
@@ -1,83 +1,79 @@
 var projects = [
     {
         id: 0,
-        name: "TODO w/ React",
-        github: "https://github.com/wustlcse204/09-todo-react-kaitlinaclark/",
+        name: "TODO List",
         site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
         langs: "React",
-        description: "todo"
+        description: "Add/delete/sort TODO list items. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
+},
+{
+    id: 10,
+    name: "Brand Redesign (Trivago)",
+    site: "https://wustlcse204.github.io/06-brand-redesign-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "Redesigned Trivago website. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
+},
+{
+    id: 11,
+    name: "Instagram Clone",
+    site: "https://wustlcse204.github.io/05-instagram-clone-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "Front-end clone of 2009 version of Instagram Desktop site. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
+},
+{
+    id: 12,
+    name: "Image Gallery",
+    site: "https://wustlcse204.github.io/07-image-gallery-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "View a gallery of images. Click photos for full-screen view. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
 },
 {
     id: 1,
     name: "Weather Widget",
     github: "https://github.com/kaitlinaclark/Weather-Widget",
     site: "",
-    langs: "",
-    description: "weather"
+    langs: "HTML/CSS, JavaScript",
+    description: "Randomized weather information accessed using course-provided API. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
     id: 2,
     name: "Geoloc",
     github: "https://github.com/kaitlinaclark/Geoloc",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "Angular, SocketIO, Google Maps API",
-    description: "todo"
+    site: "",
+    langs: "AngularCLI, SocketIO, Google Maps API",
+    description: "Location based Chat Room. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
     id: 3,
     name: "Chat Room",
     github: "https://github.com/kaitlinaclark/ChatRoom",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "NodeJS",
-    description: "todo"
+    site: "",
+    langs: "HTML/CSS, Bootstrap, NodeJS",
+    description: "Simple Chat Room with ability to communicate from different machines. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
     id: 4,
     name: "JSCalendar",
     github: "https://github.com/kaitlinaclark/JSCalendar",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "JavaScript, PHP, MySQL",
-    description: "todo"
-},
-{
-    id: 5,
-    name: "JSCalculator",
-    github: "https://github.com/kaitlinaclark/JS-Calculator",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "JavaScript, HTML, CSS",
-    description: "todo"
+    site: "",
+    langs: "HTML/CSS, JavaScript, PHP, MySQL",
+    description: "Simple Calendar with ability to add events. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
     id: 6,
     name: "News Site",
     github: "https://github.com/kaitlinaclark/NewsSite-PHP-MySQL",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "PHP, MySQL, HTML, CSS",
-    description: "todo"
+    site: "",
+    langs: "HTML/CSS, PHP, MySQL",
+    description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
     id: 7,
     name: "File Sharing Site",
     github: "https://github.com/kaitlinaclark/FileShare-PHP",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "PHP, MySQL, HTML, CSS",
-    description: "todo"
-},
-{
-    id: 8,
-    name: "Birthday Card CSS",
-    github: "https://github.com/kaitlinaclark/HTML-CSS",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "HTML, CSS",
-    description: "todo"
-},
-{
-    id: 9,
-    name: "PHP Calculator",
-    github: "https://github.com/kaitlinaclark/PHP_Calculator",
-    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
-    langs: "PHP",
-    description: "todo"
+    site: "",
+    langs: "HTML/CSS, MySQL",
+    description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 }]
 
 export default projects
