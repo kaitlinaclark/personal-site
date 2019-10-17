c3c465e1e5f9641302f586edcab7db02e30b765c
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-10-17 11:42:28 -0500

    altering portfolio

--- src/files/list-of-projects.js
+++ src/files/list-of-projects.js
@@ -7,41 +7,11 @@ var projects = [
         description: "Add/delete/sort TODO list items. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
 },
 {
-    id: 10,
-    name: "Brand Redesign (Trivago)",
-    site: "https://wustlcse204.github.io/06-brand-redesign-kaitlinaclark/",
-    langs: "HTML/CSS, Bootstrap",
-    description: "Redesigned Trivago website. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
-},
-{
-    id: 11,
-    name: "Instagram Clone",
-    site: "https://wustlcse204.github.io/05-instagram-clone-kaitlinaclark/",
-    langs: "HTML/CSS, Bootstrap",
-    description: "Front-end clone of 2009 version of Instagram Desktop site. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
-},
-{
-    id: 12,
-    name: "Image Gallery",
-    site: "https://wustlcse204.github.io/07-image-gallery-kaitlinaclark/",
-    langs: "HTML/CSS, Bootstrap",
-    description: "View a gallery of images. Click photos for full-screen view. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)"
-},
-{
-    id: 1,
-    name: "Weather Widget",
-    github: "https://github.com/kaitlinaclark/Weather-Widget",
-    site: "",
-    langs: "HTML/CSS, JavaScript",
-    description: "Randomized weather information accessed using course-provided API. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
-},
-{
-    id: 2,
-    name: "Geoloc",
-    github: "https://github.com/kaitlinaclark/Geoloc",
-    site: "",
-    langs: "AngularCLI, SocketIO, Google Maps API",
-    description: "Location based Chat Room. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+    id: 13,
+    name: "Job Scraper",
+    site: "https://wustlcse204.github.io/09-todo-react-kaitlinaclark/",
+    langs: "HTML/CSS, Python, React",
+    description: "Scraps Google Search for jobs provide in search bar. Only 3 unique searches every hour."
 },
 {
     id: 3,
@@ -60,20 +30,58 @@ var projects = [
     description: "Simple Calendar with ability to add events. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
 },
 {
-    id: 6,
-    name: "News Site",
-    github: "https://github.com/kaitlinaclark/NewsSite-PHP-MySQL",
-    site: "",
-    langs: "HTML/CSS, PHP, MySQL",
-    description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+    id: 12,
+    name: "Image Gallery",
+    site: "https://wustlcse204.github.io/07-image-gallery-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "View a gallery of images. Click photos for full-screen view. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)."
 },
 {
-    id: 7,
-    name: "File Sharing Site",
-    github: "https://github.com/kaitlinaclark/FileShare-PHP",
-    site: "",
-    langs: "HTML/CSS, MySQL",
-    description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
-}]
+    id: 10,
+    name: "Brand Redesign (Trivago)",
+    site: "https://wustlcse204.github.io/06-brand-redesign-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "Redesigned Trivago website. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)."
+},
+{
+    id: 11,
+    name: "Instagram Clone",
+    site: "https://wustlcse204.github.io/05-instagram-clone-kaitlinaclark/",
+    langs: "HTML/CSS, Bootstrap",
+    description: "Front-end clone of 2009 version of Instagram Desktop site. Made in CSE 204 with Prof Kraig Hufstedler (WUSTL 2018)."
+},
+// {
+//     id: 1,
+//     name: "Weather Widget",
+//     github: "https://github.com/kaitlinaclark/Weather-Widget",
+//     site: "",
+//     langs: "HTML/CSS, JavaScript",
+//     description: "Randomized weather information accessed using course-provided API. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+// },
+// {
+//     id: 2,
+//     name: "Geoloc",
+//     github: "https://github.com/kaitlinaclark/Geoloc",
+//     site: "",
+//     langs: "AngularCLI, SocketIO, Google Maps API",
+//     description: "Location based Chat Room. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+// },
+// ,{
+//     id: 6,
+//     name: "News Site",
+//     github: "https://github.com/kaitlinaclark/NewsSite-PHP-MySQL",
+//     site: "",
+//     langs: "HTML/CSS, PHP, MySQL",
+//     description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+// }
+// ,{
+//     id: 7,
+//     name: "File Sharing Site",
+//     github: "https://github.com/kaitlinaclark/FileShare-PHP",
+//     site: "",
+//     langs: "HTML/CSS, MySQL",
+//     description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+// }
+]
 
 export default projects
