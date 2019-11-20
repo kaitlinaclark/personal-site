626b0331888994e91b2255d9263697a98b97bda7
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-11-20 15:02:02 -0600

    changing project setup

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -4,20 +4,20 @@ import './Project.css';
 class Project extends Component {
    render(){
        return (
-        <div className="border border-info rounded-right project">
-            <div className="card" style="width: 18rem;">
-                <img src="..." className="card-img-top" alt="..."/> 
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.name}</h5>
-                    <p className="card-text">{this.props.d}</p>
-                    <i>Languages used: {this.props.langs}</i>
-                    <div className="d-flex justify-content-end align-items-center">
-                        <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
-                        <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
-                    </div>
+        
+        <div className="card" >
+            <img src="..." className="card-img-top" alt="..."/> 
+            <div className="card-body">
+                <h5 className="card-title">{this.props.name}</h5>
+                <p className="card-text">{this.props.d}</p>
+                <i>Languages used: {this.props.langs}</i>
+                <div className="d-flex justify-content-end align-items-center">
+                    <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
+                    <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
                 </div>
             </div>
         </div>
+        
     );
    } 
 }
