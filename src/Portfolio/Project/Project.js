654519ce5037b296be9bef1cf8d677d0c2395647
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-11-20 15:10:35 -0600

    fixing description

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -6,13 +6,11 @@ class Project extends Component {
        return (
         
         <div className="card" >
-            <img src="..." className="card-img-top" alt="..."/> 
+            <img src="../../files/todo-img.png" className="card-img-top" alt="..."/> 
             <div className="card-body">
                 <h5 className="card-title">{this.props.name}</h5>
-                <p className="card-text">{this.props.d}
-                
-                <i>Languages used: {this.props.langs}</i></p>
-                
+                <p className="card-text">{this.props.d}</p>
+                <p className="card-text"><i>Languages used: {this.props.langs}</i></p>
                 <div className="d-flex justify-content-end align-items-center">
                     <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
                     <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
