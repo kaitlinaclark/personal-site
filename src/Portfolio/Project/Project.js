ca2ca09d9d4f76227cf78f022fd42cad608ce334
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-11-20 15:05:07 -0600

    making everthing work

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -9,8 +9,10 @@ class Project extends Component {
             <img src="..." className="card-img-top" alt="..."/> 
             <div className="card-body">
                 <h5 className="card-title">{this.props.name}</h5>
-                <p className="card-text">{this.props.d}</p>
-                <i>Languages used: {this.props.langs}</i>
+                <p className="card-text">{this.props.d}
+                
+                <i>Languages used: {this.props.langs}</i></p>
+                
                 <div className="d-flex justify-content-end align-items-center">
                     <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
                     <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
