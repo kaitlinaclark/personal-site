d45483f955e1aec02c908af36fe943b9c900e033
Kaitlin A Clark <kaitlinaclark@wustl.edu>
2020-06-23 09:22:14 -0500

    word wrap

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -8,8 +8,8 @@ class Project extends Component {
         <div className="card" >
             <div className="card-body">
                 <h5 className="card-title">{this.props.name}</h5>
-                <p className="card-text">{this.props.d}</p>
-                <p className="card-text"><i>Languages used: {this.props.langs}</i></p>
+                <div className="card-text">{this.props.d}</div>
+                <div className="card-text"><i>Languages used: {this.props.langs}</i></div>
                 <div className="d-flex justify-content-end align-items-center">
                     <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
                     <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
