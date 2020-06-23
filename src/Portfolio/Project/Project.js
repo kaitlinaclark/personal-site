26e91cac30cc99da925288f2dbe15d2ded69aba7
Kaitlin A Clark <kaitlinaclark@wustl.edu>
2020-06-22 23:41:23 -0500

    no pic

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -6,7 +6,6 @@ class Project extends Component {
        return (
         
         <div className="card" >
-            <img src="..." className="card-img-top" alt="..."/> 
             <div className="card-body">
                 <h5 className="card-title">{this.props.name}</h5>
                 <p className="card-text">{this.props.d}
