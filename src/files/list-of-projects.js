5ba120b5a295309cee30b560ec5d3244cf526c34
Kaitlin A Clark <kaitlinaclark@wustl.edu>
2020-06-22 23:10:54 -0500

    update projs

--- src/files/list-of-projects.js
+++ src/files/list-of-projects.js
@@ -8,31 +8,32 @@ var projects = [
 // },
 {
     id: 2,
-    name: "Geoloc",
-    github: "https://github.com/kaitlinaclark/Geoloc",
-    site: "",
-    langs: "AngularCLI, SocketIO, Google Maps API",
+    name: "Location-based Chat",
+    github: "https://github.com/kaitlinaclark/geoloc",
+    site: null,
+    langs: "Angular, SocketIO, Google Maps API",
     description: "Ability to join chatrooms based on current location and communicate with people around you with some arbitrary username."
 },
 {
     id: 3,
     name: "Chat Room",
-    github: "https://github.com/kaitlinaclark/ChatRoom",
-    site: "http://ec2-13-59-151-216.us-east-2.compute.amazonaws.com:8000/",
+    github: "https://github.com/kaitlinaclark/simple-chat",
+    site: null,
     langs: "HTML/CSS, Bootstrap, NodeJS",
     description: "Simple Chat Room with ability to communicate from different machines."
 },
 {
     id: 4,
-    name: "JSCalendar",
-    github: "https://github.com/kaitlinaclark/JSCalendar",
-    site: "",
+    name: "Virtual Calendar",
+    github: "https://github.com/kaitlinaclark/simple-calendar",
+    site: null,
     langs: "HTML/CSS, JavaScript, PHP, MySQL",
     description: "Simple JavaScript Calendar with ability to add/delete/edit events. MySQL database installed so calendars/events can be shared."
 },
 {
     id: 12,
     name: "Image Gallery",
+    github: null,
     site: "https://wustlcse204.github.io/07-image-gallery-kaitlinaclark/",
     langs: "HTML/CSS, Bootstrap",
     description: "View a gallery of images. Click photos for full-screen view."
@@ -40,6 +41,7 @@ var projects = [
 {
     id: 10,
     name: "Brand Redesign (Trivago)",
+    github: null,
     site: "https://wustlcse204.github.io/06-brand-redesign-kaitlinaclark/",
     langs: "HTML/CSS, Bootstrap",
     description: "Redesigned Trivago website."
@@ -47,26 +49,27 @@ var projects = [
 {
     id: 11,
     name: "Instagram Clone",
+    github: null,
     site: "https://wustlcse204.github.io/05-instagram-clone-kaitlinaclark/",
     langs: "HTML/CSS, Bootstrap",
     description: "Front-end clone of 2009 version of Instagram Desktop site."
 },
-// {
-//     id: 1,
-//     name: "Weather Widget",
-//     github: "https://github.com/kaitlinaclark/Weather-Widget",
-//     site: "",
-//     langs: "HTML/CSS, JavaScript",
-//     description: "Randomized weather information accessed using course-provided API. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
-// },
-// ,{
-//     id: 6,
-//     name: "News Site",
-//     github: "https://github.com/kaitlinaclark/NewsSite-PHP-MySQL",
-//     site: "",
-//     langs: "HTML/CSS, PHP, MySQL",
-//     description: "Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
-// }
+{
+    id: 1,
+    name: "Simple Weather Widget",
+    github: "https://github.com/kaitlinaclark/weather-widget",
+    site: "https://kaitlinaclark.github.io/weather-widget",
+    langs: "HTML/CSS, JavaScript",
+    description: "Randomized weather information accessed using course-provided API. Made in CSE 330 with Prof Todd Sproull (WUSTL 2017)."
+ },
+ {
+    id: 6,
+    name: "News Site",
+    github: "https://github.com/kaitlinaclark/news-php-sql",
+    site: null,
+    langs: "HTML/CSS, PHP, MySQL",
+    description: "Simple news site with ability to create, delete, and edit articles and comments."
+ }
 // ,{
 //     id: 7,
 //     name: "File Sharing Site",
