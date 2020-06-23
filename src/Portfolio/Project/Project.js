7135f13d9dbb15915cb25a9520060b219f122082
Kaitlin A Clark <kaitlinaclark@wustl.edu>
2020-06-23 10:01:06 -0500

    changing style

--- src/Portfolio/Project/Project.js
+++ src/Portfolio/Project/Project.js
@@ -5,15 +5,14 @@ class Project extends Component {
    render(){
        return (
         
-        <div className="card" >
-            <div className="card-body">
-                <h5 className="card-title">{this.props.name}</h5>
-                <div className="card-text">{this.props.d}</div>
-                <div className="card-text"><i>Languages used: {this.props.langs}</i></div>
-                <div className="d-flex justify-content-end align-items-center">
-                    <a className="btn btn-primary btn-xs"  href={this.props.github} target="_blank">Github</a>
-                    <a className="btn btn-primary btn-xs"  href={this.props.site} target="_blank">Live Site</a>
-                </div>
+        <div className="row" >
+            <div className="col-12">
+                <h5 >{this.props.name}
+                  <a className="fa fa-md fa-github"  href={this.props.github} target="_blank"> </a>
+                  <a className="fa fa-md fa-link"  href={this.props.site} target="_blank"> </a>
+               </h5>
+                <div >{this.props.d}</div>
+                <div ><i>Languages used: {this.props.langs}</i></div>
             </div>
         </div>
         
