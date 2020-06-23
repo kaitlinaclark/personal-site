3b572b7171810e6550ef9c47688a42b1efcf71fc
Kaitlin A Clark <kaitlinaclark@wustl.edu>
2020-06-23 09:32:57 -0500

    no social medias

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -18,7 +18,7 @@ function AboutMe() {
                  in St. Louis, I have learned the art of problem-solving; breaking extremely complex problems down into actionable tasks. Something I just 
                  learned in class has become a way of life with a community I cannot imagine not being a part of. I love computer science, programming,
                  building applications and I aim to use my skills and passion to aid people. 
-                 But don't just take my word for it. Check out my resume and feel free to connect with me on any of my social medias linked below.</div>
+                 But don't just take my word for it. Check out my resume and feel free to connect with me on any of my socials linked below.</div>
                 
             </div>
         </div>
